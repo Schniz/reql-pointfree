5de79dba40bc7889fadc7e9f8c805e9b369d8ff2
Gal Schlezinger <galstar@spitfire.co.il>
2015-11-14 00:49:39 +0200

    decided on a name

--- test/composing.js
+++ test/composing.js
@@ -1,4 +1,4 @@
-import { compose, add, toArray, take, filter, run, map } from '../lib'
+import { db, compose, execute, add, toArray, take, table, filter, map } from '../lib'
 import r from 'rethinkdb'
 let upper = t => t.toUpperCase()
 let exclaim = t => t + '!'
@@ -8,12 +8,15 @@ let exclaimRethink = add('!')
 let getFullName = map(t => t('firstName').add(' ').add(t('lastName')))
 let getFirst = take(0)
 let filterGal = filter({ firstName: 'Gal' })
-let exec = t => run(r.conn)(t)
+let users = table('users')
+let rdb = db('shiftlift')
 
 describe('compose', () => {
+  let reql
   before((done) => {
     r.connect().then((conn) => {
       r.conn = conn
+      reql = execute(conn)
       done()
     })
   })
@@ -31,15 +34,21 @@ describe('compose', () => {
   })
 
   it('should work with sequences in rethink', (done) => {
-    let c = compose(head, toArray, exec, map(exclaimRethink), getFullName, filterGal)
-    let usersTable = r.db('shiftlift').table('users')
-    c(usersTable).should.eventually.equal('Gal Schlezinger!').and.notify(done)
+    let c = compose(head, toArray, reql, map(exclaimRethink), getFullName, filterGal, users)
+    c(rdb).should.eventually.equal('Gal Schlezinger!').and.notify(done)
   })
 
   it('should work with objects in rethink', (done) => {
-    let c = compose(exec, getLastName, getFirst, filterGal)
-    let usersTable = r.db('shiftlift').table('users')
-    c(usersTable).should.eventually.equal('Schlezinger').and.notify(done)
+    let c = compose(reql, getLastName, getFirst, filterGal, users, rdb)
+    c().should.eventually.equal('Schlezinger').and.notify(done)
+  })
+
+  it('should work with twice composing', (done) => {
+    let allGals = compose(filterGal, users, rdb)
+    let lastName = compose(getLastName, getFirst, allGals)
+    //let executor = compose(exec, lastName)
+    //executor().should.eventually.equal('Schlezinger').and.notify(done)
+    reql(lastName).should.eventually.equal('Schlezinger').and.notify(done)
   })
 
   after((done) => {

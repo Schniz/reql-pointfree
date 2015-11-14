e7c0588b5676f57cd772482f5909726bdf7d4a82
Gal Schlezinger <galstar@spitfire.co.il>
2015-11-14 14:51:58 +0200

    Big breaking changes. for working with rethinkdbdash

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # reql-pointfree
 Functional RethinkDB functions for point free programming
 
+works on the RethinkDB official driver and rethinkdbdash
+
+## Installation
+just
+```
+npm install --save reql-pointfree
+``` 
+and you're set
+
 ## Usage
 ```js
-import { db, compose, filter, take, table, add, execute } from 'reql-pointfree'
+import createReQL from 'reql-pointfree'
+const { db, compose, filter, take, table, add, execute } = createReQL(r) // the rethink driver
 
 // create a `reql` function to call your ReQLs
-let reql = execute(r.connection)
+let reql = execute(r.connection) // rethinkdbdash is just execute()
 
 // some functions
 let exclaim = add('!') // r.something().add('!')
@@ -28,24 +38,61 @@ reql(getSuperheroesFromUsers).then(...)
 this is the concept. you don't have to compose everything, but you CAN compose things. which is great IMO.
 
 ## Functions
-### `db`
-### `table`
-### `limit`
-### `orderBy`
 ### `add`
-### `eqJoin`
-### `run`
-### `toArray`
-### `filter`
-### `get`
-### `coerceTo`
-### `desc`
-### `asc`
-### `execute`
-### `compose`
-### `isReQL`
-### `map`
-### `take`
+Just like the official driver says.
 
-## Note about mapping
-the `map` method calls `map` on `SEQUENCE|ARRAY|TABLE|STREAM` and `do` on other cases.
+### `asc`
+Just like the official driver says.
+
+### `coerceTo`
+Just like the official driver says.
+
+### `compose`
+composes functions from right to left.
+supports Promises (thenables), ReQL and normal functions
+
+### `db`
+Just like the official driver says.
+
+### `desc`
+Just like the official driver says.
+
+### `eqJoin`
+Just like the official driver says.
+
+### `execute`
+creates a `reql` function, which can be `compose`d too.
+
+### `filter`
+Just like the official driver says.
+
+### `get`
+Just like the official driver says.
+
+### `isReQL`
+tells if the argument is a ReQL query
+
+### `limit`
+Just like the official driver says.
+
+### `map`
+calls `map` on `SEQUENCE|ARRAY|TABLE|STREAM` and `do` on other cases.
+
+### `orderBy`
+Just like the official driver says.
+
+### `run`
+Just like the official driver says.
+
+### `table`
+Just like the official driver says.
+
+### `take`
+for taking stuff out of an object. just like
+
+```js
+r.table('users').find({ isSuperhero: true })('name')
+//                       take('name')(reql)-^^^^^^^^
+```
+### `toArray`
+Just like the official driver says.
--- test/composing.js
+++ test/composing.js
@@ -1,57 +1,73 @@
-import { db, compose, execute, add, toArray, take, table, filter, map } from '../lib'
-import r from 'rethinkdb'
+import rethinkdbDriver from 'rethinkdb'
+import pfReql from '../lib'
+const rethinkdbdashDriver = require('rethinkdbdash')()
 let upper = t => t.toUpperCase()
 let exclaim = t => t + '!'
-let getLastName = take('lastName')
-let head = t => t[0]
-let exclaimRethink = add('!')
-let getFullName = map(t => t('firstName').add(' ').add(t('lastName')))
-let getFirst = take(0)
-let filterGal = filter({ firstName: 'Gal' })
-let users = table('users')
-let rdb = db('shiftlift')
 
-describe('compose', () => {
-  let reql
-  before((done) => {
-    r.connect().then((conn) => {
-      r.conn = conn
+function test({ text, r, pre }) {
+  describe(text, () => {
+    let reql, db, compose, execute, add, take, table, filter, map, getLastName, exclaimRethink, getFullName, getFirst, filterGal, users, rdb
+    before((done) => pre(r).then((conn) => {
+      let pf = pfReql(r)
+      db = pf.db
+      compose = pf.compose
+      execute = pf.execute
+      add = pf.add
+      take = pf.take
+      table = pf.table
+      filter = pf.filter
+      map = pf.map
+      getLastName = take('lastName')
+      exclaimRethink = add('!')
+      getFullName = map(t => t('firstName').add(' ').add(t('lastName')))
+      getFirst = take(0)
+      filterGal = filter({ firstName: 'Gal' })
+      users = table('users')
+      rdb = db('shiftlift')
       reql = execute(conn)
       done()
+    }))
+
+    it('should work with regular functions', () => {
+      let c = compose(exclaim, upper)
+      c('text').should.equal('TEXT!')
+    })
+
+    it('should work with regular functions and promises', (done) => {
+      let asyncText = t => Promise.resolve(t + ' World')
+      let exclaimPromise = t => Promise.resolve(exclaim(t))
+      let c = compose(exclaimPromise, upper, asyncText)
+      c('hello').should.eventually.equal('HELLO WORLD!').and.notify(done)
+    })
+
+    it('should work with sequences in rethink', (done) => {
+      let c = compose(reql, take(0), map(exclaimRethink), getFullName, filterGal, users)
+      c(rdb).should.eventually.equal('Gal Schlezinger!').and.notify(done)
+    })
+
+    it('should work with objects in rethink', (done) => {
+      let c = compose(getLastName, take(0), filterGal, users, rdb)
+      reql(c).should.eventually.equal('Schlezinger').and.notify(done)
+    })
+
+    it('should work with twice composing', (done) => {
+      let allGals = compose(filterGal, users, rdb)
+      let lastName = compose(getLastName, getFirst, allGals)
+      reql(lastName).should.eventually.equal('Schlezinger').and.notify(done)
     })
   })
+}
 
-  it('should work with regular functions', () => {
-    let c = compose(exclaim, upper)
-    c('text').should.equal('TEXT!')
+describe('compose', () => {
+  test({
+    text: 'using rethinkdb',
+    pre: (r) => r.connect().then(e => r.conn = e),
+    r: rethinkdbDriver
   })
 
-  it('should work with regular functions and promises', (done) => {
-    let asyncText = t => Promise.resolve(t + ' World')
-    let exclaimPromise = t => Promise.resolve(exclaim(t))
-    let c = compose(exclaimPromise, upper, asyncText)
-    c('hello').should.eventually.equal('HELLO WORLD!').and.notify(done)
-  })
-
-  it('should work with sequences in rethink', (done) => {
-    let c = compose(head, toArray, reql, map(exclaimRethink), getFullName, filterGal, users)
-    c(rdb).should.eventually.equal('Gal Schlezinger!').and.notify(done)
-  })
-
-  it('should work with objects in rethink', (done) => {
-    let c = compose(reql, getLastName, getFirst, filterGal, users, rdb)
-    c().should.eventually.equal('Schlezinger').and.notify(done)
-  })
-
-  it('should work with twice composing', (done) => {
-    let allGals = compose(filterGal, users, rdb)
-    let lastName = compose(getLastName, getFirst, allGals)
-    //let executor = compose(exec, lastName)
-    //executor().should.eventually.equal('Schlezinger').and.notify(done)
-    reql(lastName).should.eventually.equal('Schlezinger').and.notify(done)
-  })
-
-  after((done) => {
-    r.conn.close().then(() => done())
+  test({
+    text: 'using rethinkdbdash',
+    pre: (r) => Promise.resolve(r),
+    r: rethinkdbdashDriver
   })
 })
